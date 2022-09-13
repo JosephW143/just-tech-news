d385b34101e95913d8f1b5eedc370ad9f90eec1c
JosephW143 <josephwright1436@gmail.com>
2022-09-12 23:48:13 -0400

    login and logout

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'victoriaKnows',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 const hbs = exphbs.create({});
  
@@ -15,6 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(require('./controllers/'));
 
